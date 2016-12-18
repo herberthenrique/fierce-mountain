d1f44a6e09c5385aa1f1248cbb1441af39ed0311
Herbert Henrique <eu@herberthenrique.com>
2016-12-18 15:57:08 -0200

    Fix de problema com headers

--- server/config/express.js
+++ server/config/express.js
@@ -35,6 +35,8 @@ export default function(app) {
   app.use(passport.initialize());
 
 
+
+
   // Persist sessions with MongoStore
   app.use(session({
     secret: config.secrets.session,
@@ -45,6 +47,21 @@ export default function(app) {
       db: 'auth-solution'
     })
   }));
+
+  // Add headers
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
 
 
   if (env === 'development' || env === 'test') {
