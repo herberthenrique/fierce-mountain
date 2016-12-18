cb10342b2b9f59c47c0ea782273d336a8de6c2d3
Herbert Henrique <eu@herberthenrique.com>
2016-12-18 16:01:11 -0200

    outro fix

--- server/config/express.js
+++ server/config/express.js
@@ -51,7 +51,7 @@ export default function(app) {
   // Add headers
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
