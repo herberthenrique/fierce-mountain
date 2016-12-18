c1f26759f8c31c9f9c53c4991a7b5994ed641843
Herbert Henrique <eu@herberthenrique.com>
2016-12-18 16:52:26 -0200

    adding authorization to allowed headers

--- server/config/express.js
+++ server/config/express.js
@@ -52,7 +52,7 @@ export default function(app) {
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
